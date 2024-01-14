c214064ba68d385d2d0bb45bc543970123429901
hugosuarezglez <hugosuarez@uoc.edu>
2024-01-14 20:22:36 +0100

    add

--- R/SVM_RCC.R
+++ R/SVM_RCC.R
@@ -1,12 +1,11 @@
 modelo_SVM <- function(data, data_labels) {
-ruta <- system.file('extdata', package = 'svmRCC')
+#ruta <- system.file('extdata', package = 'svmRCC')
 
 #### modelo
 
-
-modelo <- readRDS(file=paste0(ruta,"/modelo_SVM_843.Rda"))
-maximo <- readRDS(file=paste0(ruta,"/maximo_train.Rda"))
-minimo <- readRDS(file=paste0(ruta,"/minimo_train.Rda"))
+# modelo <- readRDS(file=paste0(ruta,"/modelo_SVM_843.Rda"))
+# maximo <- readRDS(file=paste0(ruta,"/maximo_train.Rda"))
+# minimo <- readRDS(file=paste0(ruta,"/minimo_train.Rda"))
 
 #########################################
 # modelo <- readRDS(system.file("extdata", "modelo_SVM_843.Rda", package = "svmRCC"))
@@ -15,8 +14,34 @@ minimo <- readRDS(file=paste0(ruta,"/minimo_train.Rda"))
 # genes_modelo <- readRDS(system.file("extdata", "genes_modelo_SVM.Rda", package = "svmRCC"))
 
 
+
+
 ## seleccionar datos
-genes_modelo <- readRDS(file=paste0(ruta,"/genes_modelo_SVM.Rda"))
+# genes_modelo <- readRDS(file=paste0(ruta,"/genes_modelo_SVM.Rda"))
+
+
+
+#### ARCHIVOS
+
+
+# URL del archivo en GitHub
+url_modelo <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/modelo_SVM_843.Rda"
+url_maximo <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/maximo_train.Rda"
+url_minimo <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/minimo_train.Rda"
+url_genes <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/genes_modelo_SVM.Rda"
+
+# Descargar el archivo RDA
+maximo <- httr::GET(url_maximo, httr::write_disk("maximo_train.Rda", overwrite = TRUE))
+minimo <- httr::GET(url_minimo, httr::write_disk("minimo_train.Rda", overwrite = TRUE))
+modelo <- httr::GET(url_modelo, httr::write_disk("modelo_SVM_843.Rda", overwrite = TRUE))
+genes_modelo <- httr::GET(url_genes, httr::write_disk("genes_modelo_SVM.Rda", overwrite = TRUE))
+# Cargar el contenido del archivo RDA en R
+maximo <- readRDS("maximo_train.Rda")
+minimo <- readRDS("minimo_train.Rda")
+modelo <- readRDS("modelo_SVM_843.Rda")
+genes_modelo <- readRDS("genes_modelo_SVM.Rda")
+
+
 
 data <- data[,genes_modelo]
 
