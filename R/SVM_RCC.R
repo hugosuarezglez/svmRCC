523a021636e5608d564c87f4a4dc2f4aa1343567
hugosuarezglez <hugosuarez@uoc.edu>
2024-01-15 01:56:58 +0100

    añadir archivos

--- R/SVM_RCC.R
+++ R/SVM_RCC.R
@@ -1,45 +1,29 @@
-modelo_SVM <- function(data, data_labels) {
-#ruta <- system.file('extdata', package = 'svmRCC')
-
-#### modelo
-
-# modelo <- readRDS(file=paste0(ruta,"/modelo_SVM_843.Rda"))
-# maximo <- readRDS(file=paste0(ruta,"/maximo_train.Rda"))
-# minimo <- readRDS(file=paste0(ruta,"/minimo_train.Rda"))
-
-#########################################
-# modelo <- readRDS(system.file("extdata", "modelo_SVM_843.Rda", package = "svmRCC"))
-# maximo <- readRDS(system.file("extdata", "maximo_train.Rda", package = "svmRCC"))
-# minimo <- readRDS(system.file("extdata", "minimo_train.Rda", package = "svmRCC"))
-# genes_modelo <- readRDS(system.file("extdata", "genes_modelo_SVM.Rda", package = "svmRCC"))
+modelo_SVM <- function(data, data_labels, ruta_modelo, ruta_genes) {
+  modelo <- readRDS(file = ruta_modelo)
+  genes_modelo <- readRDS(file = ruta_genes)
 
 
-
-
-## seleccionar datos
-# genes_modelo <- readRDS(file=paste0(ruta,"/genes_modelo_SVM.Rda"))
-
-
-
+  minimo <- 0
+  maximo <- 19.15297
 #### ARCHIVOS
-
-
-# URL del archivo en GitHub
-url_modelo <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/modelo_SVM_843.Rda"
-url_maximo <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/maximo_train.Rda"
-url_minimo <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/minimo_train.Rda"
-url_genes <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/genes_modelo_SVM.Rda"
-
-# Descargar el archivo RDA
-maximo <- httr::GET(url_maximo, httr::write_disk("maximo_train.Rda", overwrite = TRUE))
-minimo <- httr::GET(url_minimo, httr::write_disk("minimo_train.Rda", overwrite = TRUE))
-modelo <- httr::GET(url_modelo, httr::write_disk("modelo_SVM_843.Rda", overwrite = TRUE))
-genes_modelo <- httr::GET(url_genes, httr::write_disk("genes_modelo_SVM.Rda", overwrite = TRUE))
-# Cargar el contenido del archivo RDA en R
-maximo <- readRDS("maximo_train.Rda")
-minimo <- readRDS("minimo_train.Rda")
-modelo <- readRDS("modelo_SVM_843.Rda")
-genes_modelo <- readRDS("genes_modelo_SVM.Rda")
+#
+#
+# # URL del archivo en GitHub
+# url_modelo <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/modelo_SVM_843.Rda"
+# url_maximo <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/maximo_train.Rda"
+# url_minimo <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/minimo_train.Rda"
+# url_genes <- "https://github.com/hugosuarezglez/svmRCC/raw/main/inst/extdata/genes_modelo_SVM.Rda"
+#
+# # Descargar el archivo RDA
+# maximo <- httr::GET(url_maximo, httr::write_disk("maximo_train.Rda", overwrite = TRUE))
+# minimo <- httr::GET(url_minimo, httr::write_disk("minimo_train.Rda", overwrite = TRUE))
+# modelo <- httr::GET(url_modelo, httr::write_disk("modelo_SVM_843.Rda", overwrite = TRUE))
+# genes_modelo <- httr::GET(url_genes, httr::write_disk("genes_modelo_SVM.Rda", overwrite = TRUE))
+# # Cargar el contenido del archivo RDA en R
+# maximo <- readRDS("maximo_train.Rda")
+# minimo <- readRDS("minimo_train.Rda")
+# modelo <- readRDS("modelo_SVM_843.Rda")
+# genes_modelo <- readRDS("genes_modelo_SVM.Rda")
 
 
 
